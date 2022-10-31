f4a93a4cf03d385daf9f605fb7f9d586e04980c5
Tony <tunglin.yang1@gmail.com>
2022-11-01 02:12:39 +0800

    加入專案檔案。

---
+++ 1A2B/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace _1A2B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.lnlAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 53);
+            this.txtInput.MaxLength = 4;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 22);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(12, 96);
+            this.txtGuess.Multiline = true;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtGuess.Size = new System.Drawing.Size(263, 297);
+            this.txtGuess.TabIndex = 1;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(12, 24);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "NewGame";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(118, 51);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 3;
+            this.btnGuess.Text = "輸入";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(183, 396);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswer.TabIndex = 4;
+            this.btnAnswer.Text = "看答案";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // lnlAnswer
+            // 
+            this.lnlAnswer.AutoSize = true;
+            this.lnlAnswer.Location = new System.Drawing.Point(264, 401);
+            this.lnlAnswer.Name = "lnlAnswer";
+            this.lnlAnswer.Size = new System.Drawing.Size(33, 12);
+            this.lnlAnswer.TabIndex = 5;
+            this.lnlAnswer.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 433);
+            this.Controls.Add(this.lnlAnswer);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(319, 476);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(319, 476);
+            this.Name = "Form1";
+            this.Text = "1A2B";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnAnswer;
+        private System.Windows.Forms.Label lnlAnswer;
+    }
+}
+
---
+++ 1A2B/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1A2B
+{
+    public partial class Form1 : Form
+    {
+        private GuessNumber game;
+        public Form1()
+        {
+            InitializeComponent();
+            game = new GuessNumber();
+            txtGuess.Text = "Guess     Result";
+            game.NewGame();
+            lnlAnswer.Visible = false;
+        }
+
+
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            game.NewGame();
+        }
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            int? guessNumber = GetGuessNumber();
+
+            if (guessNumber.HasValue == false)
+            {
+                MessageBox.Show("請輸入4個不一樣的數字");
+                return;
+            }
+
+            GuessResult result = game.Guess(guessNumber.Value);
+            if (result.IsSuccess == true)
+            {
+                MessageBox.Show("您答對了");
+
+            }
+            else
+            {
+                //lnlAnswer.Text = answer;
+                txtGuess.Text += result.Hint;
+            }
+
+
+        }
+        private int? GetGuessNumber()
+        {
+            TextBox txt = this.txtInput;
+            string input = txt.Text;
+            bool repeat = false;
+            if (string.IsNullOrEmpty(input)) return null;
+            
+            bool isInt = int.TryParse(input, out int number);
+
+           
+            if(input.Length == 4)
+            {
+                if (input[0] == input[1] || input[0] == input[2] ||
+                    input[0] == input[3] || input[1] == input[2] ||
+                    input[1] == input[3] || input[2] == input[3])
+                {
+                    repeat = true;
+                }
+            }
+            
+            return (isInt && input.Length ==4 && !repeat ) ? number : (int?)null;
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            int? guessNumber = GetGuessNumber();
+
+            GuessNumber result = game;
+            lnlAnswer.Text = result.Answer;
+            lnlAnswer.Visible = true;
+        }
+    }
+
+    public class GuessNumber
+    {
+        String[] answerAr = new string[4];
+        String[] guessNumberAr = new string[4];
+        int a = 0, b = 0;
+        int guessNumber = 0;
+        string answer;
+
+        public string Answer { get; set; }
+        public void NewGame()
+        {
+            
+            int seed = Guid.NewGuid().GetHashCode();
+            var random = new Random(seed);
+            int number1 = random.Next(0, 9);
+            int number2 = random.Next(0, 9);
+            int number3 = random.Next(0, 9);
+            int number4 = random.Next(0, 9);
+
+            
+            
+            while (number2 == number1)
+            {
+                number2 = random.Next(0, 9);
+            }                    
+            while (number3 == number1 || number3 == number2)
+            {
+                number3 = random.Next(0, 9);
+            }
+            while (number4 == number1 || number4 == number2 || number4 == number3)
+            {
+                number4 = random.Next(0, 9);
+            }
+            string ans1 = number1.ToString();
+            string ans2 = number2.ToString();
+            string ans3 = number3.ToString();
+            string ans4 = number4.ToString();
+            answer = ($"{ans1}{ans2}{ans3}{ans4}");
+            Answer = answer;
+        }
+
+        public GuessResult Guess(int inputGuessNumber)
+        {
+            guessNumber = inputGuessNumber;
+            string inputGuessNumberString = inputGuessNumber.ToString();
+
+            
+            if (inputGuessNumberString[0] == inputGuessNumberString[1] || inputGuessNumberString[0] == inputGuessNumberString[2] ||
+                inputGuessNumberString[0] == inputGuessNumberString[3] || inputGuessNumberString[1] == inputGuessNumberString[2] ||
+                inputGuessNumberString[1] == inputGuessNumberString[3] || inputGuessNumberString[2] == inputGuessNumberString[3] )
+            {
+                return GuessResult.RepeatNum();
+            }
+            
+            for (int k = 0; k <= 3; k++)
+            {
+                answerAr[k] = answer.Substring(k, 1);
+            }
+            a = 0;
+            b = 0;
+            for (int m = 0; m < 4; m++)
+            {
+                for (int p = 0; p < 4; p++)
+                {
+                    if(inputGuessNumberString[m].ToString() == answerAr[p])
+                    {
+                        if(m == p)
+                        {
+                            a++;
+                        }
+                        else
+                        {
+                            b++;
+                        }
+                    }
+                }
+                if(a == 4 && b == 0)
+                {
+                    return GuessResult.Success();
+                }                        
+            }
+            return GuessResult.Failed(this.Hint);
+        }
+
+        public string Hint
+        {
+            get
+            {
+                return $"\r\n {guessNumber}       {a}A{b}B";
+            }
+        }
+    }
+
+    public class GuessResult
+    {
+        public static GuessResult Success()
+        {
+            return new GuessResult { IsSuccess = true, Hint = String.Empty };
+        }
+        public static GuessResult Failed(string errMessage)
+        {
+            return new GuessResult { IsSuccess = false, Hint = errMessage };
+        }
+        public static GuessResult RepeatNum()
+        {
+            return new GuessResult { IsSuccess = false, Hint = String.Empty };
+        }
+
+        public bool IsSuccess { get; set; }
+
+        public string Hint { get; set; }
+
+    }
+}
